6e2a3414428497a90a162c4574f2bff73f3a148d
Vinícius <santsmarcos81@gmail.com>
2024-05-14 15:35:21 -0300

    Criando a page Home

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyles from './styles/global';
-import theme from './styles/theme'
+import theme from './styles/theme';
+
+import { Home } from './pages/Home';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      Olá
+      <Home />
     </ThemeProvider>
   </React.StrictMode>,
 )
