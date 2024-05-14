29432cf358357fd1cfafbc0b3fe064fa960f9163
Vinícius <santsmarcos81@gmail.com>
2024-05-14 15:29:29 -0300

    Configurando tema global do styled-components

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyles from './styles/global';
+import theme from './styles/theme'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      Olá
+    </ThemeProvider>
   </React.StrictMode>,
 )
---
+++ src/styles/theme.js
@@ -0,0 +1,20 @@
+export default {
+    COLORS: {
+        BACKGROUND_LINEAR_GRADIENT: "linear-gradient(#f5eaf7,#dbccdc)",
+
+        BACKGROUND_900: "#232129",
+        BACKGROUND_800: "#312E38",
+        BACKGROUND_700: "#3E3B47",
+
+        WHITE: "#F4EDE8",
+
+        BLUE_300: "#a3a4db",
+        BLUE_200: "#7677ca",
+        BLUE_100: "#6061ac",
+
+        GRAY_300: "#666360",
+        GRAY_100: "#999591",
+
+        CORAL: "#f59661"
+    }
+}
