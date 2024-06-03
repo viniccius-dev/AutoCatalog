73a851acf9f800b73a4d4231a6d298b366dce085
Vinícius <santsmarcos81@gmail.com>
2024-06-03 13:48:15 -0300

    Atualizacao pagina History

--- src/pages/History/index.jsx
+++ src/pages/History/index.jsx
@@ -1,13 +1,80 @@
-import { Container } from './styles';
+import { useEffect, useState } from "react";
 
+import { FaHeart, FaRegHeart } from "react-icons/fa6";
+
+import { Container } from './styles';
 import { Header } from '../../components/Header';
 
+import { API, ApiBase } from "../../helpers/api";
+
 export function History() {
+    const [listComparison, setListComparison] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await API.renderhistory();
+                setListComparison(response.data);
+            } catch (error) {
+                console.error('Erro ao buscar dados do histórico:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
     return (
         <Container>
             <Header />
 
-            
+            <main>
+                <header>
+                    <h1>Histórico</h1>
+                    <button><FaRegHeart /> Favoritos</button>
+                </header>
+
+                <section>
+                    {
+                        !listComparison ?
+
+                        <h3>Não há nenhum histórico de comparativos</h3>
+
+                        :
+
+                        listComparison.map(comparison => (
+                            <div key={comparison.id} className="card">
+                                <div className="cars">
+                                    {comparison.car_1 && 
+                                        <img 
+                                            src={`${ApiBase}/media/vehicle/${comparison.car_1.vehicleImage}`}
+                                            alt={comparison.car_1.vehicleName}
+                                        />}     
+                                    {comparison.car_2 && 
+                                        <img 
+                                            src={`${ApiBase}/media/vehicle/${comparison.car_2.vehicleImage}`}
+                                            alt={comparison.car_2.vehicleName}
+                                        />
+                                    }     
+                                    {comparison.car_3 && 
+                                        <img 
+                                            src={`${ApiBase}/media/vehicle/${comparison.car_3.vehicleImage}`} 
+                                            alt={comparison.car_3.vehicleName}
+                                        />
+                                    }     
+                                    {comparison.car_4 && 
+                                        <img 
+                                            src={`${ApiBase}/media/vehicle/${comparison.car_4.vehicleImage}`} 
+                                            alt={comparison.car_4.vehicleName}
+                                        />
+                                    }     
+                                </div>
+                                    
+                                <h3>Comparativo</h3>
+                            </div>
+                        ))
+                    }
+                </section>
+            </main>
         </Container>
     );
 }
--- src/pages/History/styles.js
+++ src/pages/History/styles.js
@@ -1,5 +1,97 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    
+    width: 100%;
+    height: 100vh;
+
+    display: grid;
+    grid-template-rows: 90px auto;
+    grid-template-areas:
+    "header"
+    "content";
+
+    > main {
+        grid-area: content;
+        overflow-y: auto;
+        width: 625px;
+        margin: 38px auto;
+        padding: 0 10px;
+
+        header {
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+
+            padding-bottom: 20px;
+
+            border-bottom-width: 1px;
+            border-bottom-style: solid;
+            border-bottom-color: ${({ theme }) => theme.COLORS.BLUE_300};
+
+            h1 {
+                color: ${({ theme }) => theme.COLORS.BLUE_100};
+            }
+
+            button {
+                font-size: 20px;
+                color: ${({ theme }) => theme.COLORS.LIGHT_RED};
+                background: none;
+                border: none;
+
+                display: flex;
+                align-items: center;
+                gap: 5px;
+            }
+        }
+
+        section {
+            padding: 30px 0;
+            display: flex;
+            flex-direction: column;
+            gap: 15px;
+
+            > h3 {
+                width: 100%;
+                text-align: center;
+                padding: 40px 0;
+                color: ${({ theme }) => theme.COLORS.GRAY_300};
+            }
+
+            div.card {
+                width: 100%;
+                height: 60px;
+                background-color: ${({ theme }) => theme.COLORS.WHITE};
+                border: ${({ theme }) => `1px solid ${theme.COLORS.GRAY_100}`};
+                border-radius: 5px;
+                cursor: pointer;
+
+                display: grid;
+                grid-template-columns: 3fr 1fr;
+
+                h3 {
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    font-size: 21px;
+                    background-color: ${({ theme }) => theme.COLORS.BACKGROUND_600};
+                    border-left: ${({ theme }) => `1px solid ${theme.COLORS.GRAY_100}`}
+                }
+            }
+
+            div.cars {
+                display: flex;
+                justify-content: left;
+                gap: 20px;
+                padding-left: 15px;
+                align-items: center;
+
+                img {
+                    width: 90px;
+                    height: 58px;
+                    object-fit: cover;
+                    object-position: center;
+                }
+            }
+        }
+    }
 `;
